4b571fd5ff2fbca5c47988306cca4631df23f614
Paulo Barros <abrahamkroenem@gmail.com>
2018-07-16 04:57:04 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/layout/header.component';
+import { HomeModule } from './home/home.module';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    HomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/layout/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ij-layout-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
