e474d293a0e30c2ce47a96507ef29002aa4d9d86
Paulo Barros <abrahamkroenem@gmail.com>
2018-07-18 03:53:11 -0400

    parsing object from url to Job Object

--- src/app/jobs/job.service.ts
+++ src/app/jobs/job.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Job } from './job';
 import { MOCK_JOBS } from './mock.jobs';
@@ -10,13 +11,29 @@ import { MOCK_JOBS } from './mock.jobs';
 })
 export class JobService {
 
-  static _githubJobsApi = 'https://jobs.github.com/positions.json';
+  static _infinityJobsApi = 'http://127.0.0.1:4040/';
 
   constructor(
     private _httpClient: HttpClient
   ) { }
 
   getJobs(): Observable<Job[]> {
-    return of(MOCK_JOBS);
+    return this._httpClient.get(JobService._infinityJobsApi).
+      pipe(
+        map((res: Array<any>) => res.map(job =>
+          new Job(
+            job.id,
+            job.created_at,
+            job.title,
+            job.location,
+            job.type,
+            job.description,
+            job.how_to_apply,
+            job.company,
+            job.company_url,
+            job.company_logo,
+            job.url
+          )))
+      );
   }
 }
